e781c74d24cdce6030dbac825f0458cb3405cc07
Andy Hull <andyhull3@gmail.com>
2013-03-13 19:08:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfBrigade::Application.config.secret_token = '26bbc338f3c4aca84139e46602b966a76bbf713a88ddc80090cd0c23badaf89fce1d3df282e54aa1db5e427b6a6388aee78742612cba6d0170a5d7e1c0531a28'
